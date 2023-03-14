3c56b0c2457320cc56d3f79f52a1d06112eca35f
Mateus Belmonte <mateus.santos@suporterenova.onmicrosoft.com>
2023-03-14 14:30:46 -0300

    :add: migrations to MySQL

---
+++ Migrations/20230314130056_PopulaCategorias.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace apiCatalogo.Migrations
+{
+    public partial class PopulaCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Categoria(Nome, ImageUrl) Values('Bebida', 'bebidas.jpg')");
+            mb.Sql("Insert into Categoria(Nome, ImageUrl) Values('Lanches', 'lanches.jpg')");
+            mb.Sql("Insert into Categoria(Nome, ImageUrl) Values('Sobremesas', 'sobremesas.jpg')");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Categoria");
+        }
+    }
+}
---
+++ Migrations/20230314165009_PopulaProdutos.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace apiCatalogo.Migrations
+{
+    public partial class PopulaProdutos : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Produto(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId)" + 
+                "Values('Coca-Cola Diet','Refrigerante de Cola 350ml',5.45,'cocacola.jpg',50,now(),1)");
+
+            mb.Sql("Insert into Produto(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId)" + 
+                "Values('Sanduiche','Sanduiche de presunto',7.00,'sanduiche.jpg',20,now(),2)");
+            
+            mb.Sql("Insert into Produto(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId)" + 
+                "Values('Picole','Picole da Falcao',2.00,'picole.jpg',100,now(),3)");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Produto");
+        }
+    }
+}
